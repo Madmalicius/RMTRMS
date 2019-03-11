4ea22c04a8792c8d2df85da35d08aad26cee595a
Rasmus <rasmus@hvilshoej.dk>
2019-03-11 14:23:35 +0100

    Added serial field to database

--- Database/createDatabase.py
+++ Database/createDatabase.py
@@ -22,6 +22,7 @@ if __name__ == "__main__":
     CREATE TABLE IF NOT EXISTS position (
     id INTEGER PRIMARY KEY,
     name text UNIQUE NOT NULL,
+    serial text UNIQUE NOT NULL,
     positionX FLOAT NOT NULL,
     positionY FLOAT NOT NULL,
     positionZ FLOAT NOT NULL,
