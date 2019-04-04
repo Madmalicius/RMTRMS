d5f9dcf1070674ba46dc29c0b5f99f2d09d804b6
Kasper Laursen <kassen-kasper@hotmail.com>
2019-04-04 15:30:13 +0200

    Add tracker class

---
+++ Database/trackers.py
@@ -0,0 +1,21 @@
+class tracker:
+    def __init__(self, vr, tracker):
+        self.vr = vr
+        self.trackerID = tracker
+        self.serial = self.vr.devices[self.trackerID].get_serial()
+        pose = self.vr.devices[self.trackerID].get_pose_euler()
+        self.x = pose[0]
+        self.z = pose[1]
+        self.y = pose[2]
+        self.yaw = pose[3]
+        self.pitch = pose[4]
+        self.roll = pose[5]
+
+    def update_position(self):
+        pose = self.vr.devices[self.trackerID].get_pose_euler()
+        self.x = pose[0]
+        self.z = pose[1]
+        self.y = pose[2]
+        self.yaw = pose[3]
+        self.pitch = pose[4]
+        self.roll = pose[5]
