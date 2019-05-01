cb2d6c5d28ff27e1672caf9e63074851b393a60c
Kasper Laursen <kassen-kasper@hotmail.com>
2019-05-01 13:12:40 +0200

    Rename file

--- Database/RMTRMS.py
+++ Database/RMTRMS.py
@@ -3,37 +3,49 @@ from sqlite3 import Error as sqliteError
 
 
 class Tracker:
-    def __init__(self, vr, trackerID):
-        self.vr = vr
-        self.trackerID = trackerID
-        self.serial = self.vr.devices[self.trackerID].get_serial()
-        self.name = ""
-        pose = self.vr.devices[self.trackerID].get_pose_euler()
-        self.x = pose[0]
-        self.z = pose[1]
-        self.y = pose[2]
-        self.yaw = pose[3]
-        self.pitch = pose[4]
-        self.roll = pose[5]
+    def __init__(self, vr=None, db=None, trackerID=None, serial=None):
+        self.db = db
+        if vr:
+            self.active = True
+            self.vr = vr
+            self.trackerID = trackerID
+            self.serial = self.vr.devices[self.trackerID].get_serial()
+            self.update_position()
+            self.db.update_tracker_position(self)
+            self.name = db.get_tracker_name(self)
+
+            if self.name == None:
+                self.db.set_default_tracker_name(self)
+                self.name = self.db.get_tracker_name(self)
+        else:
+            self.active = False
+            self.serial = serial
+            self.name = self.db.get_tracker_name(self)
 
     def update_position(self):
-        pose = self.vr.devices[self.trackerID].get_pose_euler()
-        self.x = pose[0]
-        self.z = pose[1]
-        self.y = pose[2]
-        self.yaw = pose[3]
-        self.pitch = pose[4]
-        self.roll = pose[5]
+        if self.active is True:
+            pose = self.vr.devices[self.trackerID].get_pose_euler()
+            self.x = pose[0]
+            self.z = pose[1]
+            self.y = pose[2]
+            self.yaw = pose[3]
+            self.pitch = pose[4]
+            self.roll = pose[5]
+            
+            self.db.update_tracker_position(self)
 
 
 class Database:
-    def __init__(self, db):
+    def __init__(self, db, vr=None):
         self.databasePath = db
         try:
             self.db = sqlite3.connect(db)
         except sqliteError as e:
             print(e)
         self.curs = self.db.cursor()
+
+        if vr is not None:
+            self.vr = vr
 
     def get_module_list(self):
         """Returns a list of modules.
@@ -62,6 +74,26 @@ class Database:
             trackerList = self.curs.execute("SELECT serial FROM trackers").fetchall()
             for index, tracker in enumerate(trackerList):
                 trackerList[index] = tracker[0]
+
+            activeTrackers = []
+
+            for device in self.vr.devices:
+                if "tracker" not in device:
+                    continue
+
+                activeTrackers.append(Tracker(vr=self.vr, db=self, trackerID=device))
+
+            for tracker in activeTrackers:
+                try:
+                    trackerList.remove(tracker.serial)
+                except ValueError:
+                    pass
+
+            for index, tracker in enumerate(trackerList):
+                trackerList[index] = Tracker(db=self, serial=tracker)
+
+            trackerList.extend(activeTrackers)
+
             return trackerList
         except sqliteError as e:
             print(e)
@@ -102,19 +134,38 @@ class Database:
             ).fetchone()[0]
         except sqliteError as e:
             print(e)
+        except TypeError:
+            return None
 
-    def set_tracking_status(self, module, status):
+    def set_module_tracking_status(self, module, status):
         """Sets the tracking status of the specified module.
         
         Arguments:
             module {String} -- The module of which the status will be modified.
-            status {int} -- The tracking status of the module. 1 for tracking, 0 for not tracking.
+            status {bool} -- The tracking status of the module. 1 for tracking, 0 for not tracking.
         """
 
         try:
             self.curs.execute(
                 "UPDATE modules SET tracked=:status WHERE module=:module",
                 (status, module),
+            )
+        except sqliteError as e:
+            print(e)
+
+        self.db.commit()
+
+    def set_tracker_tracking_status(self, tracker, status):
+        """Sets the tracking status of the specified tracker in the database
+
+        Arguments:
+            tracker {Tracker} -- The tracker of which the status will be modified.
+            status {bool} -- The tracking status of the tracker.
+        """
+        try:
+            self.curs.execute(
+                "UPDATE trackers SET tracked=:status WHERE serial=:serial",
+                (status, tracker.serial),
             )
         except sqliteError as e:
             print(e)
@@ -133,6 +184,27 @@ class Database:
             self.curs.execute(
                 "UPDATE trackers SET name=:name WHERE serial=:serial",
                 (name, tracker.serial),
+            )
+        except sqliteError as e:
+            print(e)
+
+        self.db.commit()
+
+    def set_default_tracker_name(self, tracker):
+        try:
+            self.curs.execute(
+                "SELECT serial FROM trackers WHERE serial=:serial;",
+                {"serial": tracker.serial},
+            )
+
+            params = {
+                "name": "Tracker " + str(self.curs.lastrowid),
+                "serial": tracker.serial,
+            }
+
+            self.curs.execute(
+                "UPDATE trackers SET name=:name WHERE serial=:serial AND name IS NULL;",
+                params,
             )
         except sqliteError as e:
             print(e)
@@ -167,8 +239,13 @@ class Database:
             tracker {Tracker} -- The tracker whose position will be updated in the database
         """
 
+        if tracker.active == False:
+            print("Error: Tracker is not tracked\n")
+            return None
+
         params = {
             "serial": tracker.serial,
+            "active": tracker.active,
             "positionX": tracker.x,
             "positionY": tracker.y,
             "positionZ": tracker.z,
@@ -196,14 +273,15 @@ class Database:
             self.curs.execute("SELECT changes()")
             if self.curs.fetchone()[0] is 0:
                 sql = """
-                INSERT or IGNORE INTO trackers (serial,
+                INSERT INTO trackers (serial,
+                                        active,
                                         positionX,
                                         positionY,
                                         positionZ,
                                         yaw,
                                         pitch,
                                         roll)
-                VALUES(:serial, :positionX, :positionY, :positionZ, :yaw, :pitch, :roll);
+                VALUES(:serial, :active, :positionX, :positionY, :positionZ, :yaw, :pitch, :roll);
                 """
                 try:
                     self.curs.execute(sql, params)
@@ -217,7 +295,7 @@ class Database:
                 }
 
                 sql = """
-                UPDATE or IGNORE trackers SET name=:name WHERE serial = :serial AND name IS NULL;
+                UPDATE trackers SET name=:name WHERE serial = :serial AND name IS NULL;
                 """
 
                 self.curs.execute(sql, params2)
@@ -225,6 +303,19 @@ class Database:
             print(e)
 
         self.db.commit()
+
+    def remove_tracker(self, tracker):
+        """Removes the specified tracker from the database
+
+        parameters:
+            tracker {Tracker} -- The tracker will be deleted
+        """
+
+        try:
+            self.curs.execute("DELETE FROM trackers WHERE serial = :serial", {"serial":tracker.serial})
+            self.db.commit()
+        except sqliteError as e:
+            print(e)
 
     def check_close(self):
         """Checks the close field of the database
