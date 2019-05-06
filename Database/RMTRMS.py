a86f8914367c04f77226b64762d1f2e8fbfd6cfb
Rasmus <rasmus@hvilshoej.dk>
2019-05-06 15:07:46 +0200

    Fix remove tracker before assignment

--- Database/RMTRMS.py
+++ Database/RMTRMS.py
@@ -257,7 +257,7 @@ class Database:
 
         try:
             self.curs.execute(
-                "UPDATE modules SET tracker=? WHERE tracker=:tracker",
+                "UPDATE modules SET tracker=NULL WHERE tracker=:tracker",
                 {"tracker": tracker.serial},
             )
             self.curs.execute(sql, params)
