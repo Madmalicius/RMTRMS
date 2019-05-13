687dd7039d88f2a439f40905f9673c03af61209b
Kasper Laursen <kassen-kasper@hotmail.com>
2019-05-13 12:50:39 +0200

    Add check to see if SteamVR is running

--- Program/SteamVR/configure.py
+++ Program/SteamVR/configure.py
@@ -8,6 +8,13 @@ HMD-less tracking. See README for more info.
 
 import os
 import shutil
+import subprocess
+
+processes = subprocess.Popen(
+    "tasklist", stdin=subprocess.PIPE, stderr=subprocess.PIPE, stdout=subprocess.PIPE
+).communicate()[
+    0
+]  # Get list of all running processes
 
 steamVRSettings = os.path.join(
     os.path.dirname(os.path.realpath(__file__)), "steamvr.vrsettings"
@@ -16,15 +23,23 @@ steamVRSettings = os.path.join(
 steamVRPath = "C:\Program Files (x86)\Steam\config\steamvr.vrsettings"
 steamVRBakPath = "C:\Program Files (x86)\Steam\config\steamvr.vrsettings.bak"
 
+
 def configure():
+
+    if b"vrmonitor.exe" in processes:
+        print("Cannot modify files while SteamVR is running.")
+        return 1
+
     try:
         if not os.path.isfile(steamVRBakPath):
             shutil.copyfile(steamVRPath, steamVRBakPath)
             print("Back-up created.\n")
         shutil.copyfile(steamVRSettings, steamVRPath)
         print("Configured!\n")
+        return 0
     except shutil.Error as e:
         print(e)
+
 
 if __name__ == "__main__":
     configure()
--- Program/SteamVR/restore.py
+++ Program/SteamVR/restore.py
@@ -8,14 +8,28 @@ See README for more info.
 
 import os
 import shutil
+import subprocess
+
+processes = subprocess.Popen(
+    "tasklist", stdin=subprocess.PIPE, stderr=subprocess.PIPE, stdout=subprocess.PIPE
+).communicate()[
+    0
+]  # Get list of all running processes
 
 steamVRPath = "C:\Program Files (x86)\Steam\config\steamvr.vrsettings"
 steamVRBakPath = "C:\Program Files (x86)\Steam\config\steamvr.vrsettings.bak"
 
+
 def restore():
+    if b"vrmonitor.exe" in processes:
+        print("Cannot modify files while SteamVR is running.")
+        return 1
+
     try:
         shutil.copyfile(steamVRBakPath, steamVRPath)
         os.remove(steamVRBakPath)
+        print("Restored!")
+        return 0
     except OSError as e:
         print("ERROR: No backup found.")
 
