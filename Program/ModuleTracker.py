2ba117f1c3445b16035afece7fa897e6602cfd81
Rasmus <rasmus@hvilshoej.dk>
2019-05-22 15:43:07 +0200

    Link to GUI part of wiki

--- Program/ModuleTracker.py
+++ Program/ModuleTracker.py
@@ -515,7 +515,7 @@ if __name__ == "__main__":
     menu.add_command(
         label="Help",
         command=lambda: webbrowser.open_new_tab(
-            "https://github.com/Madmalicius/RMTRMS/wiki/For-Users"
+            "https://github.com/Madmalicius/RMTRMS/wiki/For-Users#using-the-gui"
         ),
     )
     menu.add_command(
